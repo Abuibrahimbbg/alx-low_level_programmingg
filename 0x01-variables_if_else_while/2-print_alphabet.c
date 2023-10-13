ae16492da3e3c6910cfc0afb9660aa49d2366e72
Abuibrahimbbg <abuibrahimbbg@gmail.com>
2023-10-13 03:11:53 -0700

    printing single chars

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always success (0)
+ */
+
+int main(void)
+{
+	char low_char;
+
+	low_char = 'a';
+
+	while (low_char <= 'z')
+	{
+		putchar(low_char);
+		low_char++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
