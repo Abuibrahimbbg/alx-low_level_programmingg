4eff1f7a768b032c1987fdf854341b0c794f87b0
Abuibrahimbbg <abuibrahimbbg@gmail.com>
2023-10-15 09:47:37 -0700

    Printing letters in reverse order

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,14 +8,11 @@
 
 int main(void)
 {
-	int num;
+	char letters;
 
-	for (num = 0; num <= 9; num++)
+	for (letters = 'z'; letters >= 'a'; letters--)
 	{
-		putchar(num);
+		putchar(letters);
 	}
-
 	putchar('\n');
-
 	return (0);
-}
